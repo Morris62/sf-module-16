55b7af3e9b17922930599b6bb740ef631c3c5bed
a.chaturov <chaturovai@gmail.com>
2025-04-25 18:42:56 +0300

    unit 16.2

---
+++ Module_16.Tests/CalculatorTests.cs
@@ -0,0 +1,38 @@
+﻿namespace Module_16.Tests;
+
+[TestFixture]
+public class CalculatorTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void MultiplicationMustReturnNotNullValue()
+    {
+        var calc = new Calculator();
+        Assert.That(calc.Multiply(4, 2), Is.EqualTo(8), "Значения должны быть равны");
+    }
+
+    [Test]
+    public void SubtractionMustReturnCorrectValue()
+    {
+        var calc = new Calculator();
+        Assert.That(calc.Subtraction(10, 9), Is.EqualTo(1), "Значения должны быть равны");
+    }
+
+    [Test]
+    public void DivisionMustReturnCorrectValue()
+    {
+        var calc = new Calculator();
+        Assert.That(calc.Division(10, 5), Is.EqualTo(2), "Значения должны быть равны");
+    }
+
+    [Test]
+    public void DivisionMustReturnException()
+    {
+        var calc = new Calculator();
+        Assert.Throws<DivideByZeroException>(() => calc.Division(10, 0));
+    }
+}
---
+++ Module_16/Calculator.cs
@@ -0,0 +1,8 @@
+namespace Module_16;
+
+public class Calculator
+{
+    public int Multiply(int a, int b) => (a * b);
+    public int Division(int a, int b) => (a / b);
+    public int Subtraction(int a, int b) => (a - b);
+}
