2c05eafa80afaa664edb5c74660523f5cd9b7d63
a.chaturov <chaturovai@gmail.com>
2025-04-29 18:48:53 +0300

    unit 16.5

--- Module_16/Tickets/TicketService.cs
+++ Module_16/Tickets/TicketService.cs
@@ -2,17 +2,37 @@ namespace Module_16.Tickets;
 
 public class TicketService : ITicketService
 {
-    private IEnumerable<Ticket> FakeBaseData =>
-        new List<Ticket>
-        {
-            new(1, "Москва - Санкт-Петербург", 3500),
-            new(2, "Челябинск - Магадан", 3500),
-            new(3, "Норильск - Уфа", 3500)
-        };
+    private readonly List<Ticket> FakeBaseData = new()
+    {
+        new Ticket(1, "Москва - Санкт-Петербург", 3500),
+        new Ticket(2, "Челябинск - Магадан", 3500),
+        new Ticket(3, "Норильск - Уфа", 3500)
+    };
 
     public int? GetTicketPrice(int ticketId)
     {
         var ticket = FakeBaseData.FirstOrDefault(x => x.Id == ticketId);
         return ticket?.Price ?? throw new TicketNotFoundException();
+    }
+
+    public Ticket GetTicket(int ticketId)
+    {
+        var ticket = FakeBaseData.FirstOrDefault(x => x.Id == ticketId);
+        return ticket ?? throw new TicketNotFoundException();
+    }
+
+    public void SaveTicket(Ticket ticket)
+    {
+        FakeBaseData.Add(ticket);
+    }
+
+    public void DeleteTicket(Ticket ticket)
+    {
+        FakeBaseData.Remove(ticket);
+    }
+
+    public IEnumerable<Ticket> GetAllTickets()
+    {
+        return FakeBaseData;
     }
 }
